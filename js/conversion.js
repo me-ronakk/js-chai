16887c5ccb69ebd94fe93ea1cbec5c413dcd214d
me-ronakk <ronakdahiya81@gmail.com>
2025-03-28 11:25:16 +0000

    complete

--- js/conversion.js
+++ js/conversion.js
@@ -1,11 +1,11 @@
 let score = 33
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // 33 = number
 // "33" = number
@@ -24,7 +24,31 @@ console.log(booleanisloogedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************OPERATIONS******************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = 'hello'
+let str2 = " ronak"
+
+let str3 = str1+str2
+console.log(str3);
+
+
+
+
+
 
 
