a694e828d8463f709f877620356cf42675ff22c7
me-ronakk <ronakdahiya81@gmail.com>
2025-03-28 10:33:55 +0000

    datatypes

---
+++ js/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer verion
+
+//alert(3+3) we are using node js ,not browser
+
+//ur code shld be readable
+
+let name = "ronak"
+let age = 19
+let isloggedin = true
+
+//number =>2 to power 53 (range)
+//bigint(used in trading / big market)
+//string =>""
+//boolean => true/false
+//null =>standalone value
+//undefined => not defined yet
+//symbol =>unique
+//objects
+
+console.log(typeof undefined);//out undefined
+console.log(typeof null); //out object
---
+++ js/variables.js
@@ -0,0 +1,17 @@
+const accountId = 14439 
+let accountEmail = "ronakdahiya@gmailo.com"
+var accountPass ="1234"
+accountCity = "delhi"
+
+// accountId = 2 const value can't be changed not allowed
+accountEmail = "robindahiya@g,ail.com"
+accountPass = "78983"
+accountCity = "jaipur"
+
+console.log(accountId);
+console.table([accountEmail , accountPass ,accountCity ,accountId]);
+/*
+prefer not to use var coz of
+ issue of block scope and functonAL SCOPE
+*/
+
