81b8ab565d666bb61df19ec4e50b467667457007
me-ronakk <ronakdahiya81@gmail.com>
2025-03-28 11:03:42 +0000

    conversion done

---
+++ js/conversion.js
@@ -0,0 +1,30 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// 33 = number
+// "33" = number
+// "33abc" = NaN (not a number)
+// true = 1 ; false = 0
+
+let isloggedin = 4
+let booleanisloogedIn = Boolean(isloggedin)
+console.log(booleanisloogedIn);
+
+// 1 => true ; 0 => false
+// "ronak" => true
+// " " =>false
+// 4 => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
