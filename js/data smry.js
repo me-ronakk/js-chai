8e9ade46c16f70b0f4e3170401b1b4c2d7e8936f
me-ronakk <ronakdahiya81@gmail.com>
2025-03-28 11:42:23 +0000

    SUMMARY

---
+++ js/data smry.js
@@ -0,0 +1,11 @@
+// # primitive datatypes =>
+
+// 7 category : string, number , boolean , null ,
+//  undefined , symbol, bigInt
+
+//JAVASCRIPT IS DYNAMICALLY DESIGNED
+
+// non-primitive datatype (reference type)=>
+
+// array , objects, functions
+ 
