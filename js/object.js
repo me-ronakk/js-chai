af7fdcbd8327f054acce4e9599804f6be1180b8d
me-ronakk <ronakdahiya81@gmail.com>
2025-04-04 08:55:13 +0000

    objects

---
+++ js/object.js
@@ -0,0 +1,15 @@
+//singleton
+
+//object literals
+//Object.create
+const JsUser = {
+    name: "ronak",
+    age:19,
+    location:"delhi",
+    email:"ronakdahiya81@gmail.com",
+    isLoggedin: false,
+    LastLoginDays:["monday","tuesday"]  
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
