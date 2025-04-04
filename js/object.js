6bf06cf4df207cfab51d000561c45a0bf61a3802
me-ronakk <ronakdahiya81@gmail.com>
2025-04-04 09:37:02 +0000

    object

--- js/object.js
+++ js/object.js
@@ -2,9 +2,14 @@
 
 //object literals
 //Object.create
+
+//symbol
+
+const mySym = Symbol("key1")
 const JsUser = {
     name: "ronak",
     age:19,
+    [mySym] :"key1",//use [] to make it a symbol
     location:"delhi",
     email:"ronakdahiya81@gmail.com",
     isLoggedin: false,
@@ -13,3 +18,11 @@ const JsUser = {
 
 console.log(JsUser.email)
 console.log(JsUser["email"])
+
+//functions
+
+JsUser.greetings = function(){
+  console.log("hello world");
+}
+
+console.log(JsUser.greetings());
