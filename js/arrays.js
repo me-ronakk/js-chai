93d422ad577157d45a935207ad54f30bc2334929
me-ronakk <ronakdahiya81@gmail.com>
2025-04-04 08:32:03 +0000

    arrays done

--- js/arrays.js
+++ js/arrays.js
@@ -1,6 +1,19 @@
 //array = collection of multipule items in single object
  // zero based indexing
 
-const myArray = [0,1,2,3,4,5,6,7,8]
+const myArr = [0,1,2,3,4,5]
 const frnds = ["shivam","riya","nikita","vinita"]
 console.log(frnds[2]);
+
+//arrays methods :
+
+myArr.push(6) //add values at the end
+myArr.pop() //remove last value of array
+myArr.unshift(9)//add values at the starting
+myArr.shift(9)//remove values from the starting
+console.log(myArr);
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(typeof(newArr));
