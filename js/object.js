9b8fa389a4826be3df5b4b9d160dc77fe6b83cac
me-ronakk <ronakdahiya81@gmail.com>
2025-04-04 10:38:46 +0000

    objects done

--- js/object.js
+++ js/object.js
@@ -1,7 +1,7 @@
-//singleton
+
 
 //object literals
-//Object.create
+
 
 //symbol
 
@@ -16,8 +16,8 @@ const JsUser = {
     LastLoginDays:["monday","tuesday"]  
 }
 
-console.log(JsUser.email)
-console.log(JsUser["email"])
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
 
 //functions
 
@@ -26,3 +26,43 @@ JsUser.greetings = function(){
 }
 
 console.log(JsUser.greetings());
+
+//songleton const tinderUser = new Object()
+
+const tinderUser = {}
+tinderUser.id = "123ronak"
+tinderUser.name = "ronak"
+tinderUser.isLogin = true
+
+console.log(tinderUser);
+
+const regularUser = {
+    email:"ronak@gmail.com",
+    fullname:{
+      userfullname:{
+        firstname:"hitesh",
+        lastname:"dahiya"
+
+      }
+    }
+    
+}
+
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+
+const obj3 = Object.assign({}, obj1 , obj2)
+console.log(obj3);
+
+//JSON IS WRITTEN IN FORM OF OBJECT BUT WITHOUT DECLARING NAME
+// both key value pair are treated as strigs
+//some times we also get the value of json in form of arrays too
+
+
+// {
+//  "name": "ronak",
+//  "coursename": "javascript",
+//  "price": "free"
+// }
