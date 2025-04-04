a6e15804fc117758b74acfba328c9e198b65df2f
me-ronakk <ronakdahiya81@gmail.com>
2025-04-04 06:55:00 +0000

    arrays done

---
+++ js/arrays.js
@@ -0,0 +1,6 @@
+//array = collection of multipule items in single object
+ // zero based indexing
+
+const myArray = [0,1,2,3,4,5,6,7,8]
+const frnds = ["shivam","riya","nikita","vinita"]
+console.log(frnds[2]);
