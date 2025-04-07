a4f30915b7b05fe20d8ce724903960c7d0c26b6d
me-ronakk <ronakdahiya81@gmail.com>
2025-04-07 08:54:07 +0000

    done

--- js/scopes.js
+++ js/scopes.js
@@ -1,1 +1,10 @@
-if ()
+if (true) {
+    const username = "ronak"
+    if(username === "ronak"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+   // console.log(website);   we cannot access website here coz website scope is limited inside if statement in{}
+
+}
+ //console.log(username); cannot access here coz username is limited in {}
